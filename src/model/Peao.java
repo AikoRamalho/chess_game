760f162aca3644e89a2b589e9f3aaa3f87bd9b21
giovannaBottino <giovannabottino14@gmail.com>
2020-10-31 23:41:09 -0300

    Movimento Válido do Peão

--- src/model/Peao.java
+++ src/model/Peao.java
@@ -12,7 +12,28 @@ class Peao extends Peca {
 	@Override
 	public boolean movimentoValido(int x, int y) {
 		// TODO Auto-generated method stub
-		return false;
+		int currentRow = this.getLinha();
+		int currentColumn = this.getColuna();
+		boolean move1 = false;
+        boolean move2 = false;
+		boolean diagonal = false;
+		boolean naoEMesmaCasa = !((currentRow==x) && (currentColumn==y)); //verifica se a casa de destino não é a casa que já está
+		
+		if(x > currentRow + 2) {
+			return false;
+		}
+		
+		if(this.getCor() == Cor.BRANCO) {
+			move1 = ((currentColumn == y) && (currentRow + 1 == x)); //anda uma casa
+			move2 = ((currentRow == 1) && (currentColumn == y) && (currentRow + 2 == x)); //anda 2 casas se for o primeio movimento
+			diagonal = (currentRow+1 == x && currentColumn - 1 == y) || (currentRow+1 == x && currentColumn+1 == y);
+		}
+		else {
+			move1 = ((currentColumn == y) && (currentRow - 1 == x)); //anda uma casa
+			move2 = ((currentRow == 6) && (currentColumn == y) && (currentRow - 2 == x)); //anda 2 casas se for o primeio movimento
+			diagonal = (currentRow - 1 == x && currentColumn - 1 == y) || (currentRow - 1 == x && currentColumn + 1 == y);
+		}
+		return naoEMesmaCasa && (move1 || move2 || diagonal);
 	}
 
 }
--- src/model/tempMain.java
+++ src/model/tempMain.java
@@ -1,18 +1,27 @@
 package model;
 
+import common.Cor;
+
 public class tempMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Tabuleiro tb = new Tabuleiro();
-		tb.getTabuleiro();
-		tb.setTabuleiro();
+		Tabuleiro tb = Tabuleiro.getTabuleiro();
+//		tb.setTabuleiro();
 		
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
-				System.out.println(tb.mPecas[i][j]);				
-			}
-		}
+		tb.mPecas[2][3] = new Rei(Cor.BRANCO,2,3);
+//		tb.mPecas[3][5] = new Cavalo(Cor.PRETO,2,2);
+//		tb.mPecas[0][2] = new Rainha(Cor.PRETO,0,6);
+		
+//		for(int linha = 0; linha < 8; linha++) {
+//			for(int coluna = 0; coluna < 8; coluna++) {
+//				System.out.println(tb.mPecas[linha][coluna]);				
+//			}
+//		}
+		
+		
+		System.out.println("Ta em xeque: " + ((Rei) tb.mPecas[2][3]).isXeque());
+//		System.out.println(tb.mPecas[2][3] instanceof Rei);
 	}
 
 }
